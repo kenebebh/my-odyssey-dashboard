a962b24532c01120d6d3ec87d576038056b997d0
kenebebh <kenebebhbanigo@gmail.com>
2024-09-15 12:02:58 +0100

    checking the sync

--- src/app/(dashboard)/user-management/demographics/page.tsx
+++ src/app/(dashboard)/user-management/demographics/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function DemographicsPage() {
-  return <div>Hello from Demographics page</div>;
+  return (
+    <div>
+      Hello from Demographics page. This page will hold maps and charts
+      concerning user location, types, age and other grouping methods
+    </div>
+  );
 }
