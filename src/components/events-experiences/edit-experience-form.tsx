31f57fc4587fe3a58a1e4053463510407dc7fa2e
kenebebh <kenebebhbanigo@gmail.com>
2025-01-30 01:31:51 +0100

    finished the experience ID page

--- src/components/events-experiences/edit-experience-form.tsx
+++ src/components/events-experiences/edit-experience-form.tsx
@@ -1,10 +1,11 @@
 import { IExperience } from "@/lib/types/experiences";
 import React from "react";
+import { Button } from "../ui/button";
 
 export default function EditExperienceForm({
   experienceData,
 }: {
   experienceData: IExperience;
 }) {
-  return <div>edit-experience-form</div>;
+  return <Button variant={"outline"}>Edit Experience Form</Button>;
 }
